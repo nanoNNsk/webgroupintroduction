9bc7dee323d5ab5d2c290f5573d5483fc25685a6
nanoNNsk <sibthinon@gmail.com>
2024-02-17 21:18:34 +0700

    Update config.php

--- tape/config.php
+++ tape/config.php
@@ -1,9 +1,9 @@
 <?php
 // Connect to server and select database.
-define('servername', 'localhost');
-define('username', 'root');
-define('password', '');
-define('dbname', 'myhotel');
+define('servername', 'sql205.infinityfree.com');
+define('username', 'if0_35926934');
+define('password', 'yqSseYqlyN3');
+define('dbname', 'if0_35926934_myhotel');
 
 $objCon = mysqli_connect(servername, username, password,dbname);
 ?>
